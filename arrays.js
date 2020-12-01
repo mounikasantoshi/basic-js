ff38c5a29a1cc85eb4aacb7ef07efd4804c2d523
mounikasantoshi <mounikasantoshi99@gmail.com>
2020-12-01 08:44:05 -0800

    initial

---
+++ arrays.js
@@ -0,0 +1,47 @@
+// friends=['mounika','mouni','santoshi','santu'];
+// console.log(friends);
+// console.log(friends[1]);
+// console.log(friends.length)
+// friends[friends.length]='lav';
+// console.log(friends)
+// friends[friends.length]='lav2'
+// console.log(friends)
+
+
+// friends.push("lav3")
+// console.log(friends);
+
+f=['mounika','mouni','santoshi','santu','lav'];
+// console.log(f[0]);
+// console.log(f[1]);
+// console.log(f[2]);
+// console.log(f[3]);
+// console.log(f[4]);
+
+// for (i=0;i<f.length;i++){
+//     //console.log(i);
+//     console.log(f[i]);    }
+// f.forEach(n => {
+//     console.log(n);
+// });
+
+//Ms.mounika
+//Ms.mouni......
+
+// f.forEach(element => {
+//     console.log('ms.'+element)
+// });
+
+ //numbers = [1,2,3,5,6,7,8,9]
+
+// for(i=0;i<numbers.length;i++){
+//     console.log(numbers[i],10+numbers[i])
+// }
+
+//2 X 1 =2
+n = 3
+
+for(i=1;i<11;i++){
+    //console.log(n+ 'X' +i+ "=" +n*i)
+    console.log(n + " X " +i+ " = " +n*i);
+}
---
+++ basic.js
@@ -0,0 +1,38 @@
+// var name = "mounika"
+// console.log(name);
+// var names='mouni'
+// console.log(names);
+
+
+// var a=10,b=20;
+// var c=a+b;
+// console.log(c)
+
+// function greeting(name) {
+//    console.log('hai ' +name);
+// }
+
+// greeting("mounika")
+// greeting("mouni")
+
+   
+// hi, i am mouni i am a mca graduate
+// function greet(name,branch){
+//   return  ("hi, i am " +name +" i am a " +branch+ " graduate")
+// }
+// console.log (greet("mouni","mca"));
+// console.log (greet("vishnu","mca"));
+
+// function sum(a=0,b=0){
+//     return(a+b)
+// }
+// console.log(sum(10,20))
+
+// console.log(sum(40))
+
+// console.log(sum(10,sum(20,20)));
+
+// console.log(sum(10,20,30));
+
+//arrays
+
